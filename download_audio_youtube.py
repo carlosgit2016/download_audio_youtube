85ecfc37a04407a6e5752d4b7df9bc2fa899d15b
Carlos Gabriel Goiani Flor <carlosggflor@gmail.com>
2020-07-13 01:54:36 -0300

    Removed unused split name

--- download_audio_youtube.py
+++ download_audio_youtube.py
@@ -8,12 +8,9 @@ bestAudio = video.getbestaudio()
 print("Best audtio founded: ", bestAudio.bitrate, bestAudio.extension, bestAudio.filename)
 print("Downloading................")
 bestAudio.download()
-# bestAudioFileName = "".join(str(bestAudio.filename).split())
-# os.rename(bestAudio.filename, bestAudioFileName)
 print("Downloading................OK")
 print("Converting to MP3................")
 inputFile = str(bestAudio.filename)
-# bestAudioTitle = "".join(str(bestAudio.title).split())
 outputFileMp3 = str(bestAudio.title) + ".mp3"
 command = f"ffmpeg -i \"{inputFile}\" -vn -ar 44100 -ac 2 -b:a 192k \"{outputFileMp3}\""
 os.system(command)
